9ad368155bd07fcb405b14bbd2d53237414853db
Takhlees Fatima <takhleesfatima2003@gmail.com>
2023-09-02 00:56:06 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Internship Tasks</h1>
+         <a href="http://localhost:3000/"> Task 1</a>
+         <a href="http://localhost:3000/"> Task 2</a>
+         <a href="http://localhost:3000/"> Task 3</a>
+         <a href="http://localhost:3000/"> Task 4</a>
+         <a href="http://localhost:3000/"> Task 5</a>
+         <a href="http://localhost:3000/"> Task 6</a>
+         <a href="http://localhost:3000/"> Task 7</a>
     </div>
   );
 }
