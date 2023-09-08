2cb367dca3b31df77e03e624aafae403830d2839
Takhlees Fatima <takhleesfatima2003@gmail.com>
2023-09-08 19:28:44 +0500

    committee

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ function App() {
   return (
     <div className="app">
       <h1>Internship Tasks</h1>
-         <a href="http://localhost:3000/"> Task 1</a>
-         <a href="http://localhost:3000/"> Task 2</a>
-         <a href="http://localhost:3000/"> Task 3</a>
-         <a href="http://localhost:3000/"> Task 4</a>
-         <a href="http://localhost:3000/"> Task 5</a>
-         <a href="http://localhost:3000/"> Task 6</a>
-         <a href="http://localhost:3000/"> Task 7</a>
-    </div>
+         <a href="https://registration-form-dnlwra5xm-takhlees.vercel.app"> Task 1</a>
+         <a href="https://landing-page-rust-sigma.vercel.app"> Task 2</a>
+         <a href="https://count-down-timer-ten.vercel.app"> Task 3</a>
+         <a href="https://currency-converter-d9zl.vercel.app"> Task 4</a>
+         <a href="https://login-page-c434.vercel.app"> Task 5</a>
+         <a href="https://calculators-red.vercel.app"> Task 6</a>
+     </div>
   );
 }
 
