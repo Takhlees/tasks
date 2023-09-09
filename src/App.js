d3b7727bbb5b01a24c69e7e83ce6d276894312e6
Takhlees Fatima <takhleesfatima2003@gmail.com>
2023-09-09 13:21:38 +0500

    commi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <h1>Internship Tasks</h1>
-         <a href="https://registration-form-dnlwra5xm-takhlees.vercel.app"> Task 1</a>
+         <a href="https://registration-form-ivory.vercel.app"> Task 1</a>
          <a href="https://landing-page-rust-sigma.vercel.app"> Task 2</a>
          <a href="https://count-down-timer-ten.vercel.app"> Task 3</a>
          <a href="https://currency-converter-d9zl.vercel.app"> Task 4</a>
